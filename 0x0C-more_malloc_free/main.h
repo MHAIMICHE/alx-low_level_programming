84ebbd3543318d6cecc7ddc3bfd367260b11a891
Mouna Haimiche <haimiche.mouna@gmail.com>
2023-04-13 17:54:29 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void init(char *str, int l);
+void _print(char *str, int l);
+int check_for_digits(char **av);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 #endif
